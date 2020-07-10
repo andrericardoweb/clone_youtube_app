b07a8fdd0ed9b409c5681b8699e2518dcf2a72dc
Andre Ricardo <andrericardoweb@gmail.com>
2020-07-09 21:46:17 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# clone_youtube_app
+ Recreating interface and consuming YouTube API - Udemy Course, Jamilton Damasceno
---
+++ android/app/src/main/java/com/andrericardoweb/clone_youtube_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.andrericardoweb.clone_youtube_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
